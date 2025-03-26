858e52653dae332f9b0985624b01de4a41821c94
Rohit Kumar <102956102+R0HITKrs@users.noreply.github.com>
2025-03-26 22:21:12 +0530

    new change

--- src/components/Dashboard/Sidebar.js
+++ src/components/Dashboard/Sidebar.js
@@ -3,7 +3,7 @@ import { List, ListItem, ListItemText } from '@mui/material';
 
 export default function Sidebar({ activePage }) {
   return (
-    <div className="sidebar">
+    <div className="Sidebar">
       <List>
         <ListItem button selected={activePage === 'details'}>
           <ListItemText primary="Details" />
